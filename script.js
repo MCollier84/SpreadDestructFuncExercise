a45247bc15b7eef3a320ecb2802821bb54070674
Maurice Collier <mauricecollier84@yahoo.com>
2021-10-21 10:07:46 -0400

    Added the bonus section of the exercise.

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 const JPDinos = [
     `Velociraptors`,
     `Tyrannosaurus Rex`,
-    `Dilophosaurs`,
+    `Dilophosaurs`
 
 ];
 
 console.log(`========`);
+
 // 1b
 function seeDinos(x, ...y){
     console.log(x);
@@ -28,8 +29,60 @@ console.log(jPCharacters);
 
 console.log(`=======`);
 
+// 2b
+
 seeCharacters = ({ellieSattler}) => {
     console.log(ellieSattler);
 }
 seeCharacters(jPCharacters);
 
+console.log(`=======`);
+
+// Bonus
+// 3a
+
+const jurassicParkMovies = [
+        {
+          one: "Jurassic Park",
+          two: "The Lost World: Jurassic Park",
+          three: "Jurassic Park III"
+        },
+        {
+          four: "Jurassic World",
+          five: "Jurassic World: Fallen Kingdom",
+          six: "Jurassic World: Dominion"
+        }
+      ];
+
+// 3b
+seeJPMovies = ([{one, two, three}, {four, five, six}]) => {
+    console.log(one);
+   console.log(two);
+   console.log(three);
+   console.log(four);
+   console.log(five);
+   console.log(six);
+  }
+  seeJPMovies(jurassicParkMovies);
+  /*
+Jurassic Park
+The Lost World: Jurassic Park
+Jurassic Park III
+Jurassic World
+Jurassic World: Fallen Kingdom
+Jurassic World: Dominion
+*/
+
+console.log(`=======`);
+// OR
+
+seeJPMovies = ({one, two, three}, {four, five, six}) => {
+    console.log(one);
+   console.log(two);
+   console.log(three);
+   console.log(four);
+   console.log(five);
+   console.log(six);
+  }
+  seeJPMovies(...jurassicParkMovies);
+  
